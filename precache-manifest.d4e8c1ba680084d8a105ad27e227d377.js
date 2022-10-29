1fd74950b2cfa03c85b9492a9b9c3ec6b829455e
JAI PRAKASH KUMAR <aryansingh879791@gmail.com>
2022-10-29 23:06:23 +0530

    Updates

---
+++ precache-manifest.d4e8c1ba680084d8a105ad27e227d377.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46de85a497861cd842b94c7b1e443f1",
+    "url": "/ParkingSlotBookingSystem/index.html"
+  },
+  {
+    "revision": "1525e34f8653fa904cf4",
+    "url": "/ParkingSlotBookingSystem/static/css/2.560b9325.chunk.css"
+  },
+  {
+    "revision": "6be5f6094525b45c67f8",
+    "url": "/ParkingSlotBookingSystem/static/css/main.a233bdb1.chunk.css"
+  },
+  {
+    "revision": "1525e34f8653fa904cf4",
+    "url": "/ParkingSlotBookingSystem/static/js/2.8ad4d688.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/ParkingSlotBookingSystem/static/js/2.8ad4d688.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be5f6094525b45c67f8",
+    "url": "/ParkingSlotBookingSystem/static/js/main.5bf8e271.chunk.js"
+  },
+  {
+    "revision": "a31c6eef9497c6e130a7",
+    "url": "/ParkingSlotBookingSystem/static/js/runtime-main.e210d128.js"
+  }
+]);
